61db6eae3e0518bd456d52fbc2843267c583d545
LHP <1339003591@qq.com>
2020-10-15 02:01:16 +0800

    单例模式

---
+++ DesignPattern/Singleton/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Start.");
+            //Singleton obj1 = Singleton.getInstance();
+            //Singleton obj2 = Singleton.getInstance();
+            //if (obj1 == obj2)
+            //{
+            //    Console.WriteLine("obj1与obj2是相同的实例。");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("obj1与obj2是不同的实例。");
+            //}
+            //Console.WriteLine("End.");
+            //Console.ReadKey();
+
+            #region Q3问题测试
+
+            Console.WriteLine("Start.");
+            for(int i=1;i<1000; i++)
+            {
+                Console.WriteLine(i);
+                
+                object obj1= new object();
+                object obj2 = new object();
+                new Task(() => { obj1 = Singleton.getInstance(); }).Start();
+                new Task(() => { obj2 = Singleton.getInstance(); }).Start();
+
+                Thread.Sleep(100);
+
+                if (obj1 != obj2)
+                {
+                    Console.WriteLine("obj1与obj2是不同的实例。");
+                    break;
+                }
+                else
+                {
+                    ((Singleton)obj1).Dispose();
+                }
+            }
+            
+            Console.WriteLine("End.");
+            Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
---
+++ DesignPattern/Singleton/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton singleton = new Singleton();
+        private Singleton()
+        {
+            Console.WriteLine("生成了一个实例。");
+        }
+        public static Singleton getInstance()
+        {
+            return singleton;
+        }
+
+        #region Q3
+        //private static Singleton singleton = null;
+        //private Singleton()
+        //{
+        //    Console.WriteLine("生成了一个实例。");
+        //}
+        //public static Singleton getInstance()
+        //{
+        //    if (singleton == null)
+        //    {
+        //        singleton = new Singleton();
+        //    }
+        //    return singleton;
+        //}
+
+        public void Dispose()
+        {
+            IDisposable disposable = singleton as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+        }
+
+        #endregion
+
+    }
+}
